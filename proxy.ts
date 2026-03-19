e69d1d447440919c1181ed14b2f299c60a145e9d
kellynil-addu <kang1@addu.edu.ph>
2026-03-19 11:28:17 +0000

    Added proxy.ts to support GitHub Codespaces

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,19 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
+const codespaceHost = 'super-guacamole-4jw9pqjrw5j5cj6jq-3000.app.github.dev'; 
+
 export function proxy(req: NextRequest) {
   // For github codespace only:
   // Overwrites origin header to the appropriate url instead of localhost
+
+  const forwardedHost = req.headers.get('x-forwarded-host');
   
-  console.log("origin is " + req.headers.get('origin'));
-  console.log("forwarded-host is " + req.headers.get('x-forwarded-host'));
-  const headers = new Headers(req.headers);
-  const host = 'super-guacamole-4jw9pqjrw5j5cj6jq-3000.app.github.dev';
-  headers.set('origin', `https://${host}`);
-  headers.set('x-forwarded-host', host);
-  return NextResponse.next({ request: { headers } });
+  if (codespaceHost === forwardedHost) {
+    console.log("i see ur in github codespaces")
+    const headers = new Headers(req.headers);
+    headers.set('origin', `https://${codespaceHost}`);
+    headers.set('x-forwarded-host', codespaceHost);
+    return NextResponse.next({ request: { headers } });
+  }
 }
