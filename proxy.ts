df066249c9d5330ab4c3af43e116179977e543d6
kellynil-addu <kang1@addu.edu.ph>
2026-03-19 11:28:17 +0000

    Added proxy.ts to support GitHub Codespaces

--- proxy.ts
+++ proxy.ts
@@ -6,6 +6,10 @@ const codespaceHost = 'super-guacamole-4jw9pqjrw5j5cj6jq-3000.app.github.dev';
 export function proxy(req: NextRequest) {
   // For github codespace only:
   // Overwrites origin header to the appropriate url instead of localhost
+<<<<<<< HEAD
+=======
+  // From https://github.com/vercel/next.js/discussions/62050#discussioncomment-10725955
+>>>>>>> ceb054b (Added proxy.ts to support GitHub Codespaces)
 
   const forwardedHost = req.headers.get('x-forwarded-host');
   
